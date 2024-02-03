e973a1f0218cd87b521212b3f4f6cc6356580fed
TahaManna07 <TahaManna07>
2024-02-04 00:06:15 +0100

    LastVersion

---
+++ GestionEquipe/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+open module ma.enset.examenjdbcfxsdia {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    //opens ma.enset.examenjdbcfxsdia to javafx.fxml;
+    exports ma.enset.examenjdbcfxsdia;
+}
