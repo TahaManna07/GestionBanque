d47efa01c54b78c1e6a4f896ce9d17deac7704bb
Taha Souhail Manna <43857744+TahaManna07@users.noreply.github.com>
2024-02-04 00:12:12 +0100

    Delete GestionEquipe directory

--- GestionEquipe/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module ma.enset.examenjdbcfxsdia {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    //opens ma.enset.examenjdbcfxsdia to javafx.fxml;
-    exports ma.enset.examenjdbcfxsdia;
-}
